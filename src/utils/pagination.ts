e7ee12556e86a095b23555eff258ba852151270c
Lukáš Janeček <lukas.janecek1@ibm.com>
2024-11-05 10:15:14 +0100

    fix(auth): fix lastUpdatedAt for api key
    Signed-off-by: Lukáš Janeček <lukas.janecek1@ibm.com>

--- src/utils/pagination.ts
+++ src/utils/pagination.ts
@@ -46,7 +46,7 @@ export async function getListCursor<T extends BaseEntity>(
   const aftr = after ? await repo.findOneOrFail({ id: after }, { filters }) : undefined;
   const bfr = before ? await repo.findOneOrFail({ id: before }, { filters }) : undefined;
   const cursor = await repo.findByCursor(where, {
-    ...(aftr ? { first: limit } : {}),
+    ...(!bfr ? { first: limit } : {}),
     ...(bfr ? { last: limit } : {}),
     before: bfr && { [snakeToCamel(order_by)]: bfr[snakeToCamel(order_by) as keyof T], id: bfr.id },
     after: aftr && {
